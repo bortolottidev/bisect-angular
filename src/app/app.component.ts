2431802900a9f5a5217b7e4deb8902ead5d3aa1d
bortolotti.dev <29543986+bortolottidev@users.noreply.github.com>
2024-01-07 23:45:09 +0100

    feat(app): move links to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,24 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'bisect-angular';
+  links = [
+    { title: 'Explore the Docs', link: 'https://angular.dev' },
+    {
+      title: 'Learn with Tutorials',
+      link: 'https://angular.dev/tutorials',
+    },
+    { title: 'CLI Docs', link: 'https://angular.dev/tools/cli' },
+    {
+      title: 'Angular Language Service',
+      link: 'https://angular.dev/tools/language-service',
+    },
+    {
+      title: 'Angular DevTest',
+      link: 'https://angular.dev/tools/devtools',
+    },
+  ];
 }
