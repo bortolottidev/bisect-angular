f808dabfa3335e9925e871b9559beaedc94f23ba
bortolotti.dev <29543986+bortolottidev@users.noreply.github.com>
2024-01-11 00:50:28 +0100

    feat(app): move constants into app.constants

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Link } from './link';
 import { LinkService } from './link.service';
+import { A_RANDOM_DEV, MY_APP_TITLE } from './app.constants';
 
 @Component({
   selector: 'app-root',
@@ -19,10 +20,10 @@ import { LinkService } from './link.service';
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  title = 'Bisect Angular Test App';
+  title = MY_APP_TITLE;
   links: WritableSignal<Array<Link>> = signal([]);
   myLinks: Signal<Array<Omit<Link, 'author'>>> = computed(() =>
-    this.links().filter(({ author }) => author === 'daniele'),
+    this.links().filter(({ author }) => author === A_RANDOM_DEV),
   );
 
   constructor(private linkService: LinkService) {}
---
+++ src/app/app.constants.ts
@@ -0,0 +1,2 @@
+export const A_RANDOM_DEV = 'daniele';
+export const MY_APP_TITLE = 'Bisect Angular Test App';
